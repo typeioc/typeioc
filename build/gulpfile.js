1fd3e3582a7815768dca90809f2086ec20808a45
Max Gherman <german.maxim@gmail.com>
2014-07-29 23:57:02 +1000

    Removed logging

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,11 +1,8 @@
 
 'use strict';
 
-var path = require('path');
 var exec = require('gulp-exec');
-var child_process = require('child_process');
 var gulp = require('gulp');
-var util = require('gulp-util');
 var eventStream = require('event-stream');
 var typescript = require('gulp-tsc');
 var remove = require('rimraf');
@@ -33,7 +30,7 @@ gulp.task('tsc-lib-all', ['tsc-lib', 'tsc-lib-js'], function() {
 
 gulp.task('tsc-lib-js', ['clean-lib-js'], function() {
 
-    var stream = eventStream.concat(
+    eventStream.concat(
         gulp.src(definitionSources, { read: false }),
         gulp.src(typescriptSources, { read: false })
     )
@@ -42,14 +39,14 @@ gulp.task('tsc-lib-js', ['clean-lib-js'], function() {
             module: 'commonjs',
             target: 'ES5',
             sourcemap : false,
-            pathFilter: function (path) { return path.replace(/^src/, libJSDestination) }
+            pathFilter: function (path) { return path.replace(/^src/, libJSDestination); }
         }))
         .pipe(gulp.dest('../'));
 });
 
 gulp.task('tsc-lib', ['clean-lib'], function() {
 
-    var stream = eventStream.concat(
+    eventStream.concat(
         gulp.src(definitionSources, { read: false }),
         gulp.src(typescriptSources, { read: false })
     )
@@ -58,7 +55,7 @@ gulp.task('tsc-lib', ['clean-lib'], function() {
         module: 'commonjs',
         target: 'ES5',
         sourcemap : true,
-        pathFilter: function (path) { return path.replace(/^src/, libDestination) }
+        pathFilter: function (path) { return path.replace(/^src/, libDestination); }
     }))
     .pipe(gulp.dest('../'));
 });
@@ -78,21 +75,3 @@ gulp.task('tsc-tests', function() {
     gulp.src(testSources, { read: false })
         .pipe(exec(typescriptCommandSM + ' <%= file.path %>'));
 });
-
-
-
-gulp.task('default', function() {
-
-    gulp.watch([definitionSources, typescriptSources, testSources], function(event) {
-        util.log(util.colors.blue(['File', event.path, 'was', event.type,', compiling...'].join(' ')));
-
-        child_process.exec(typescriptCommand + event.path, function(error, stdout, stderr) {
-            util.log(util.colors.blue('done'));
-
-            if (error !== null) {
-                util.log(util.colors.red('exec error: ' + error));
-            }
-        });
-    });
-});
-
--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 var libFolder = process.env.TYPEIOC_COV ? 'lib-cov' : 'lib';
 
-console.log('Library path : ' +libFolder);
-
 var builder =  require(['.', libFolder, 'scaffold'].join('/'));
 var types = require(['.', libFolder, 'types/index'].join('/'));
 var exceptions = require(['.', libFolder, 'exceptions/index'].join('/'));
