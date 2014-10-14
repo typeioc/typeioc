7f238ecdff3d6989fa4a815173d46b49df8057e4
Max Gherman <german.maxim@gmail.com>
2014-10-14 23:38:46 +1100

    added copyright

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -6,21 +6,24 @@ var gulp = require('gulp');
 var eventStream = require('event-stream');
 var typescript = require('gulp-tsc');
 var remove = require('rimraf');
+var header = require('gulp-header');
+var pkg = require('../package.json');
+var fs = require('fs');
 
 var typescriptSources = '../src/**/*.ts';
 var definitionSources = '../d.ts/typeioc*.d.ts';
 var testSources = '../test/**/*.ts';
 var libDestination = 'lib';
 var libFiles = '../lib';
+var libFilesHeader = '../lib/**/*.js';
+var copyrightPath = '../COPYRIGHT';
 var typescriptCommand = 'tsc --target ES5  --module commonjs ';
 var typescriptCommandSM = typescriptCommand + ' --sourcemap ';
-
 
 
 gulp.task('build', ['build-lib', 'build-tests'], function() {
 
 });
-
 
 gulp.task('build-lib', ['clean-lib'], function() {
 
@@ -36,6 +39,16 @@ gulp.task('build-lib', ['clean-lib'], function() {
         pathFilter: function (path) { return path.replace(/^src/, libDestination); }
     }))
     .pipe(gulp.dest('../'));
+});
+
+gulp.task('header', function() {
+
+    var banner = fs.readFileSync(copyrightPath) + '\n\n';
+
+    gulp.src(libFilesHeader)
+        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(gulp.dest(libFiles));
+
 });
 
 gulp.task('clean-lib', function(cb) {
