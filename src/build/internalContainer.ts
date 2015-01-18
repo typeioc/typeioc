3fd98d87d631993e06a3f21cf237f6e4680d81d8
Max Gherman <german.maxim@gmail.com>
2015-01-18 17:51:25 +1100

    Dynamic dependency resolution. Rebuild for typescript 1.4.1.0

--- src/build/internalContainer.ts
+++ src/build/internalContainer.ts
@@ -1,3 +1,11 @@
+/*---------------------------------------------------------------------------------------------------
+ * Copyright (c) 2015 Maxim Gherman
+ * typeioc - Dependency injection container for node typescript
+ * @version v1.2.7
+ * @link https://github.com/maxgherman/TypeIOC
+ * @license (MIT) - https://github.com/maxgherman/TypeIOC/blob/master/LICENSE
+ * --------------------------------------------------------------------------------------------------*/
+
 
 'use strict';
 
