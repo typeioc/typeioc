320199137865dc95976da41b25e93e18b696f871
Max Gherman <gheran.maxim@gmail.com>
2017-08-09 14:37:15 +1000

    header update

--- src/build/invoker.ts
+++ src/build/invoker.ts
@@ -1,3 +1,11 @@
+/*---------------------------------------------------------------------------------------------------
+ * Copyright (c) 2017 Maxim Gherman
+ * typeioc - Dependency injection container for node typescript / javascript
+ * @version v2.1.1
+ * @link https://github.com/maxgherman/TypeIOC
+ * @license MIT
+ * --------------------------------------------------------------------------------------------------*/
+
 "use strict";
 
 import { Reflection } from '../utils';
