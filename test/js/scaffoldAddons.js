5db7337254f3fbe4dc65c63776343cfcf6d6bff1
Max Gherman <gheran.maxim@gmail.com>
2018-04-29 16:34:50 +1000

    removed generated files in tests

--- test/js/scaffoldAddons.js
+++
@@ -1,6 +0,0 @@
-/// <reference path='../d.ts/typeioc.addons.d.ts' />
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var addons = require('../../addons/index');
-exports.Interceptors = addons.Interceptors;
-//# sourceMappingURL=scaffoldAddons.js.map
